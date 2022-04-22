47d04d716fa3432a1335a6c3c5b9c46f6d9d5954
dung0312-ptit <dungtn312@gmail.com>
2022-04-22 14:32:25 +0700

    html mail

--- water/views.py
+++ water/views.py
@@ -1,4 +1,4 @@
-from django.core.mail import EmailMessage, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError, EmailMultiAlternatives
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils.html import strip_tags
@@ -23,7 +23,6 @@ def newmail(request):
         client_mail = request.POST.get("client_mail")
         title = request.POST.get("title")
         message = request.POST.get("message")
-        message = strip_tags(message)
         files = request.FILES.getlist('file')
 
         for file in files:
@@ -32,11 +31,13 @@ def newmail(request):
         print(title)
         print(message)
 
-        email = EmailMessage(title, message, settings.EMAIL_HOST_USER, [client_mail])
+       # email = EmailMessage(title, message, settings.EMAIL_HOST_USER, [client_mail])
+        email_msg = EmailMultiAlternatives(title, "", settings.EMAIL_HOST_USER, [client_mail])
+        email_msg.attach_alternative(message, "text/html")
         for file in files:
-            email.attach(file.name, file.read(), file.content_type)
+            email_msg.attach(file.name, file.read(), file.content_type)
         try:
-            email.send()
+            email_msg.send()
         except BadHeaderError:
             return HttpResponse('invalid header found')
         return redirect('new-mail')
